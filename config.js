9b763ed0733a3aeef8800875612d8e1294bf06c6
Hardik <29hdkjain@gmail.com>
2017-05-18 07:40:51 +0530

    Functionality done

--- config.js
+++ config.js
@@ -1,38 +1,38 @@
 function getHotels(type,start,end) {
-	$('.content p').empty();
+	$('.content p').remove();
 	$.ajax({
         url: "http://www.mocky.io/v2/591596dc100000b9027595b1",
         cache: false,
         dataType: "json"
-    }).done(function(data) {
+    }).done(function(data) {  // Logic inside the done method as suggested
         data.forEach(function(booking) {
-        	if(booking.roomType === type && start <= booking.checkIn.substring(0,2) && booking.checkIn.substring(0,2) <= end) {
+        	if(booking.roomType === type && start <= booking.checkIn.substring(0,2) && booking.checkIn.substring(0,2) <= end) {   // Check for the room Type and Date of booking
         		var days = booking.checkOut.substring(0,2) - booking.checkIn.substring(0,2);
-        		var daysPadding = days*80;
-        		var left = checkInMargin( (booking.checkIn.substring(0,2)) % 7 );
+        		var daysPadding = days*100 + 50;
+        		var styleLeft = checkInMargin( (booking.checkIn.substring(0,2)) % 7 );
         		var id = document.getElementById(booking.roomNumber);
-        		var append = '<p style="left:' + left +'px;width:'+ daysPadding + 'px;"> Booked </p>'
+        		var append = `<p style="left:${styleLeft}px;width:${daysPadding}px;"> Booked </p>`
         		$(id).append(append);
         	}
         });
     });
 
-    function checkInMargin(checkIn){
+    function checkInMargin(checkIn) {
 		switch(checkIn) {
 			case 0: 
-				return 560;
+				return 900;
 			case 6:
-				return 480;
+				return 800;
 			case 5:
-				return 400;
+				return 700;
 			case 4:
-				return 320;
+				return 530;
 			case 3:
-				return 240;
+				return 500;
 			case 2:
-				return 160;
+				return 400;
 			case 1:
-				return 80;
+				return 300;
 		}
 	}
 }
