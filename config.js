1153e7bc8635e6163377f2ef719c86097f9c2214
Hardik <29hdkjain@gmail.com>
2017-05-18 10:37:31 +0530

    Removed redundancy and optimized it a little

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 function getHotels(type, start, end) {
-  $('.booked').remove(); // Remove the previous content before inserting new one.
-  $('.header p').remove(); // Remove the previous content before inserting new one.
+  $('.booked, .header p').remove(); // Remove the previous content before inserting new one.
   $.ajax({
     url: "http://www.mocky.io/v2/591596dc100000b9027595b1",
-    cache: false,
+    cache: true,
     dataType: "json"
   }).done(function (data) {  // Logic inside the done method as suggested
     let flag = 0;
     data.forEach(function (booking) {
+      // Not creating checkInDate variable here as it would calculate everytime even if the (booking.roomType === type) returns false, 
+      // but since if any of the condition in && operator returns false, it stops execution, so if roomType returns false, it wont calculate booking.checkIn.substring(0, 2) in the if condition below.
       if (booking.roomType === type && start <= booking.checkIn.substring(0, 2) && booking.checkIn.substring(0, 2) <= end) {   // Check for the room Type and Date of booking
         if (flag === 0) { // Show Dates Row only Once
           let header;
@@ -35,15 +36,16 @@ function getHotels(type, start, end) {
 								`
           }
           const headerId = `.${booking.roomType}-header`;
-          $(headerId).append(header); // Append to maple or oak header
+          $(headerId).append(header); // Append to ${roomType} header
           flag = 1; // Stop From executing twice
         }
-        const days = booking.checkOut.substring(0, 2) - booking.checkIn.substring(0, 2); //Days booked by a particular user of a particular room
+        const checkInDate = booking.checkIn.substring(0, 2);        
+        const days = booking.checkOut.substring(0, 2) - checkInDate; //Days booked by a particular user of a particular room
         let daysWidth = days * 100; // CSS to add width to show ${days}( :D ) colspan
         if (days >= 2) {
           daysWidth = daysWidth + 100;
         }
-        const styleLeft = checkInMargin((booking.checkIn.substring(0, 2)) % 7); // CSS left property to align to which column it should adhere. % 7 as remainder would give me the day, which is same for all weeks.
+        const styleLeft = checkInMargin( checkInDate % 7); // CSS left property to align to which column it should adhere. % 7 as remainder would give me the day, which is same for all weeks.
         const id = document.getElementById(booking.roomNumber);
         const append = `<p class="booked" style="left:${styleLeft}px;width:${daysWidth}px;"> Booked </p>`
         $(id).append(append); // Add the p tag to the respective room number
