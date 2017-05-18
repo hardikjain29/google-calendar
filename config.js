0af82e0b1585b9fb92c6bb9fe0a14a66d4188fce
Hardik <29hdkjain@gmail.com>
2017-05-18 08:59:19 +0530

    Dynamic header

--- config.js
+++ config.js
@@ -1,17 +1,48 @@
 function getHotels(type,start,end) {
-	$('.content p').remove();
+	$('.booked').remove(); // Remove the previous content before inserting new one.
+	$('.header p').remove(); // Remove the previous content before inserting new one.
 	$.ajax({
         url: "http://www.mocky.io/v2/591596dc100000b9027595b1",
         cache: false,
         dataType: "json"
     }).done(function(data) {  // Logic inside the done method as suggested
+    	let flag = 0;
         data.forEach(function(booking) {
         	if(booking.roomType === type && start <= booking.checkIn.substring(0,2) && booking.checkIn.substring(0,2) <= end) {   // Check for the room Type and Date of booking
-        		var days = booking.checkOut.substring(0,2) - booking.checkIn.substring(0,2);
-        		var daysPadding = days*100 + 50;
-        		var styleLeft = checkInMargin( (booking.checkIn.substring(0,2)) % 7 );
-        		var id = document.getElementById(booking.roomNumber);
-        		var append = `<p style="left:${styleLeft}px;width:${daysPadding}px;"> Booked </p>`
+        		const days = booking.checkOut.substring(0,2) - booking.checkIn.substring(0,2);
+        		if(flag === 0) {
+        			let header;
+	        		if(start === 1) {
+		        		header = `
+									<p  style="left: 100px;">Room Number</p>
+									<p 	style="left: 200px;">1</p>
+									<p 	style="left: 300px;">2</p>
+									<p  style="left: 400px;">3</p>
+									<p  style="left: 500px;">4</p>
+									<p  style="left: 600px;">5</p>
+									<p  style="left: 700px;">6</p>
+									<p  style="left: 800px;">7</p>
+								`
+					} else {
+						header = `
+									<p  style="left: 100px;">Room Number</p>
+									<p 	style="left: 200px;">8</p>
+									<p 	style="left: 300px;">9</p>
+									<p  style="left: 400px;">10</p>
+									<p  style="left: 500px;">11</p>
+									<p  style="left: 600px;">12</p>
+									<p  style="left: 700px;">13</p>
+									<p  style="left: 800px;">14</p>
+								`
+					}	
+					const headerId = `.${booking.roomType}-header`;
+					$(headerId).append(header);
+					flag = 1;
+        		}
+        		const daysPadding = days*100;
+        		const styleLeft = checkInMargin( (booking.checkIn.substring(0,2)) % 7 );
+        		const id = document.getElementById(booking.roomNumber);
+        		const append = `<p class="booked" style="left:${styleLeft}px;width:${daysPadding}px;"> Booked </p>`
         		$(id).append(append);
         	}
         });
