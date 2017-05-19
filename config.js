316f448fc9f4dac2e148fdacfab23917173b6d76
Hardik <29hdkjain@gmail.com>
2017-05-19 10:30:58 +0530

    Show prev/next only when type selected

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ var week;  // A Global state to see which week I am on. Like react state;
 const numberOfDaysToDisplay = 7; // How many days to view in a single view. Change it to any number and that many days view will come up.
 
 function getInitial() { //A func to set the initial State
-  week = 1;
+  week = 0;
   getHotels(numberOfDaysToDisplay, week);
 }
 
 function incrementWeek() {
-  const start = (numberOfDaysToDisplay * week) + 1;
   week = week + 1;
+  const start = (numberOfDaysToDisplay * week) + 1;
   getHotels(numberOfDaysToDisplay, start);
 }
 
@@ -20,6 +20,9 @@ function decrementWeek() {
 
 function getHotels(numberOfDaysToDisplay, start) {
   const type = $('.type').val();   // Get the type from dropdown
+  if(start === 0) {
+    start = 1;  // To start it from day 1;
+  }
   const end = (numberOfDaysToDisplay - 1) + start;
   $('.booked, .header p, .typeHeading h1').remove(); // Remove the previous content before inserting new one.
   $.ajax({
@@ -30,6 +33,7 @@ function getHotels(numberOfDaysToDisplay, start) {
 
     const typeHeading = `<h1 class="type">${type.toUpperCase()}</h1>`;
     $('.typeHeading').append(typeHeading);
+    $('.change_dates').css('display','block');
     numberOfDaysToDisplay = numberOfDaysToDisplay + 1; // Add 1 to accomodate Room Number Row.
     let header = `<p style="width: ${100 / numberOfDaysToDisplay}%;left: ${100 / numberOfDaysToDisplay}%;">Room Number</p>`;
     for (let i = start; i <= end; i++) {
