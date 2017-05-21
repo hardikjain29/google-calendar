8dbb1a8b6bdee342e82fec64596c590203e8b239
Hardik <29hdkjain@gmail.com>
2017-05-21 10:52:19 +0530

    Add room numbers dynamically

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-var week;  // A Global state to see which week I am on. Like react state;
+var week;  // A Global state to see which week or page I am on. Like react state;
 const numberOfDaysToDisplay = 7; // How many days to view in a single view. Change it to any number and that many days view will come up.
 
 function getInitial() { //A func to set the initial State
   week = 0;
+  $('.content div').remove(); // Remove the previous content before inserting new one.  
   getHotels(numberOfDaysToDisplay, week);
 }
 
@@ -13,7 +14,7 @@ function incrementWeek() {
 }
 
 function decrementWeek() {
-  if(week - 1 === -1){
+  if (week - 1 === -1) {
     alert('Cannot go below this');  // Alert to not allow below date 1
     return;
   }
@@ -24,7 +25,7 @@ function decrementWeek() {
 
 function getHotels(numberOfDaysToDisplay, start) {
   const type = $('.type').val();   // Get the type from dropdown
-  if(start === 0) {
+  if (start === 0) {
     start = 1;  // To start it from day 1;
   }
   const end = (numberOfDaysToDisplay - 1) + start;
@@ -37,7 +38,7 @@ function getHotels(numberOfDaysToDisplay, start) {
 
     const typeHeading = `<h1 class="type">${type.toUpperCase()}</h1>`;
     $('.typeHeading').append(typeHeading);
-    $('.change_dates').css('display','block');
+    $('.change_dates').css('display', 'block');
     numberOfDaysToDisplay = numberOfDaysToDisplay + 1; // Add 1 to accomodate Room Number Row.
     let header = `<p style="width: ${100 / numberOfDaysToDisplay}%;left: ${100 / numberOfDaysToDisplay}%;">Room Number</p>`;
     for (let i = start; i <= end; i++) {
@@ -48,11 +49,14 @@ function getHotels(numberOfDaysToDisplay, start) {
       if (booking.roomType === type && start <= booking.checkIn.substring(0, 2) && booking.checkIn.substring(0, 2) <= end) {   // Check for the room Type and Date of booking
         const checkInDate = booking.checkIn.substring(0, 2);
         const days = booking.checkOut.substring(0, 2) - checkInDate; //Days booked by a particular user of a particular room
-        const id = document.getElementById(booking.roomNumber);
-        let dayOfWeek = checkInDate % (numberOfDaysToDisplay - 1); // If the remainder is 0
+        if (!document.getElementById(booking.roomNumber)) { // Check if room number present or not
+          $('.content').append(`<div id="${booking.roomNumber}"><p>${booking.roomNumber}</p></div>`); // If not present, Add it.
+        }
+        let dayOfWeek = checkInDate % (numberOfDaysToDisplay - 1); // -1 to leave the room number cloumn and start from days column.
         if (dayOfWeek === 0) {
           dayOfWeek = numberOfDaysToDisplay - 2; // If 0, It is the end column
         }
+        const id = document.getElementById(booking.roomNumber);
         const append = `<p class="booked" style="width: ${(100 / numberOfDaysToDisplay) * days}%;left: ${(100 / numberOfDaysToDisplay) * dayOfWeek}%;"> Booked </p>`;
         $(id).append(append); // Add the p tag to the respective room number
       }
